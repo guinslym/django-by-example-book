8e7984d310b0115e541c9cc8014460ab625e7224
Guinsly Mondesir <guinslym @gmail.com>
2016-04-17 18:26:41 -0400

    create templatetags part 1

--- chap_3_blog_extending/mysite/blog/templatetags/blog_tags.py
+++ chap_3_blog_extending/mysite/blog/templatetags/blog_tags.py
@@ -1,4 +1,5 @@
 from django import template
+from django.db.models import Count
 
 register = template.Library()
 
@@ -8,4 +9,15 @@ from ..models import Post
 def total_posts():
     return Post.published.count()
 
+@register.inclusion_tag('blog/post/latest_posts.html')
+def show_latest_posts(count=5):
+    latest_posts = Post.published.order_by('-publish')[:count]
+    return {'latest_posts': latest_posts}
 
+@register.assignment_tag
+def get_most_commented_posts(count=5):
+    return Post.published.annotate(total_comments=Count('comments')).order_by('-total_comments')[:count]
+
+@register.assignment_tag
+def get_less_commented_posts(count=3):
+    return Post.published.annotate(total_comments=Count('comments')).order_by('total_comments')[:count]
