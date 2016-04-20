c5764d07ed8238bd7043bf9d840cc3108723f944
Guinsly Mondesir <guinslym @gmail.com>
2016-04-19 21:44:19 -0400

    adding Flash Messages in the app

--- chap_4_social_website/bookmarks/account/views.py
+++ chap_4_social_website/bookmarks/account/views.py
@@ -4,6 +4,7 @@ from django.contrib.auth import authenticate, login
 from . forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from .models import Profile
 
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 
 # Create your views here.
@@ -63,7 +64,9 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated successfully')
     else:
+        messages.success(request, 'Error updating your profile')
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(
         instance=request.user.profile)
