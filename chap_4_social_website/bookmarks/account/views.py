987f26501e82e726f75682984b82fb44959b7c90
Guinsly Mondesir <guinslym@users.noreply.github.com>
2016-07-02 21:43:04 -0400

    new day

--- chap_4_social_website/bookmarks/account/views.py
+++ chap_4_social_website/bookmarks/account/views.py
@@ -14,7 +14,7 @@ def user_login(request):
         form =  LoginForm(request.POST)
         if form.is_valid():
             cd = form.cleaned_data
-            user = authenticate(username=cd['username'], 
+            user = authenticate(username=cd['username'],
                     password=cd['password'])
             if user is not None:
                 if user.is_active:
@@ -32,7 +32,7 @@ def user_login(request):
 
 @login_required
 def dashboard(request):
-    return render(request, 'account/dashboard.html', {'section':'dashboard'}) 
+    return render(request, 'account/dashboard.html', {'section':'dashboard'})
 
 def register(request):
     if request.method == 'POST':
@@ -74,6 +74,3 @@ def edit(request):
                   'account/edit.html',
                   {'user_form': user_form,
                   'profile_form': profile_form})
-
-
-
